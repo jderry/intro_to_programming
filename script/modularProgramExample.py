f571553f6b1e47fe82f272dd369fd14ef79f30fe
james r. derry <jderry@austin.utexas.edu>
2021-10-07 11:37:11 -0500

    example from chapter 39 made into modular program -- edited

--- script/modularProgramExample.py
+++ script/modularProgramExample.py
@@ -1,31 +1,26 @@
-# coding: utf-8
+
 def convertDimToFeet(inches):
     return inches/12
 def computeSurfaceArea(height, radius):
     return 3.14*radius*(radius**2 + height**2)**(1/2)
 def computeCostsByColor(area):
     return {'red': area*.10, 'blue': area*.15, 'green': area*.18}
-    
-def conePaint(coneHeight, coneDiameter):
-    radius = coneDiameter/2
-    radius = convertDimToFeet(radius)
-    height = convertDimToFeet(coneHeight)
-    area = computeSurfaceArea(height, radius)
-    costDict = computeCostsByColor(area)
-    print("The surface area of the cone is: " + str(area) + " square ft.")
+def printResults(area, costDict):
+    print(f"\nThe surface area of the cone is: {area:4.2f} square ft.")
+    print("\nPainting it:")
     for color, cost in costDict.items():
-        print(f"Painting it in {color} will cost: {cost}.")
-        
-
+        print(f"{color:<6}" + " will cost" + f" ${cost:>5.2f}.")
+    
+    
 def conePaint(coneHeight, coneDiameter):
     radius = convertDimToFeet(coneDiameter/2)
     height = convertDimToFeet(coneHeight)
     area = computeSurfaceArea(height, radius)
     costDict = computeCostsByColor(area)
-    print(f"\nThe surface area of the cone is: {area:4.2f} square ft.")
-    print("\nPainting it:")
-    for color, cost in costDict.items():
-        print(f"{color:<6}" + " will cost" + f" ${cost:>5.2f}.")
+    printResults(area, costDict)
         
         
 conePaint(120, 60)
+
+
+
