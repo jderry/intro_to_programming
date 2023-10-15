55acc854ab7fcaacfdae3ed6ec978a2c2c2de184
james r. derry <jderry@austin.utexas.edu>
2023-10-15 14:27:48 -0500

    myModule and conepaint

--- current_class_files/myModule.py
+++ current_class_files/myModule.py
@@ -7,6 +7,16 @@ def getPerGCContent(nuclStr:str) -> float:
         28.571428571428573
     '''
     # garbage filter
+    assert isinstance(nuclStr, str),\
+            "the function accepts only clean nucleotide strings."
+    badValues = {}
+    for index, char in enumerate(nuclStr):
+        if char not in 'acgtACGT':
+            badValues[index] = nuclStr[index]
+    assert not badValues,\
+            f"values not nucleotides in these index positions {badValues}"
+    # garbage filter
+    
     return 100*(nuclStr.count('G')+nuclStr.count('C'))/len(nuclStr)
 
 def revCompl(nuclStr:str) -> str:
@@ -15,6 +25,16 @@ def revCompl(nuclStr:str) -> str:
         'tgtaatc'
     '''
     # garbage filter
+    assert isinstance(nuclStr, str),\
+            "the function accepts only clean nucleotide strings."
+    badValues = {}
+    for index, char in enumerate(nuclStr):
+        if char not in 'acgtACGT':
+            badValues[index] = nuclStr[index]
+    assert not badValues,\
+            f"values not nucleotides in these index positions {badValues}"
+    # garbage filter
+    
     complDict = {'a':'t', 'c':'g', 'g':'c', 't':'a'}
     outputStr = ''
     for nucleotide in nuclStr:
@@ -27,6 +47,14 @@ def euclidGCD(natA:int, natB:int) -> int:
         201
     '''
     # garbage filter
+    assert isinstance(natA, int),\
+            "natA and natB must be natural numbers greater than zero."
+    assert isinstance(natB, int),\
+            "natA and natB must be natural numbers greater than zero."
+    assert (natA > 0) and (natB > 0),\
+            "natA and natB must be natural numbers greater than zero."
+    # garbage filter
+    
     while natB:
         natA, natB = natB, natA % natB
     return natA
@@ -37,6 +65,10 @@ def char_count(someStr: str) -> dict:
         {'g': 1, 'a': 3, 't': 2, 'c': 1} 
     '''
     # garbage filter
+    assert isinstance(someStr, str),\
+        "input must be a string."
+    # garbage filter
+    
     # initialization
     char_cnt_dict = {}
 
@@ -56,6 +88,10 @@ def str2num(someStr: str) -> (int, float, str):
         0.12
     '''
     # garbage filter
+    assert isinstance(someStr, str),\
+        "input must be a string."    
+    # garbage filter
+    
     try:
         outputVal = float(someStr)
 
@@ -72,6 +108,10 @@ def get_min_max(someArray:(list, tuple)) -> tuple:
         (3, 5)
     '''
     # garbage filter
+    assert isinstance(someArray, list) or isinstance(someArray, tuple),\
+        "input must be a list or tuple,\nand python must be able to logically compare their items."
+    # garbage filter
+    
     # initialization
     maxVal, minVal = float('-inf'), float('inf')
     for item in someArray:
@@ -80,3 +120,35 @@ def get_min_max(someArray:(list, tuple)) -> tuple:
         if item < minVal:
             minVal = item
     return minVal, maxVal
+    
+def makeLoL(file:str) -> list:
+    '''Given filepath to a text file holding a tabular database as input string,
+       render its contents as a list-of-lists.
+       >>>LoL = makeLoL
+    '''
+    # garbage filter
+    with open(file, 'r') as inFile:
+        LoS = inFile.read().splitlines()
+        LoL = []
+        for record in LoS:
+            LoL.append(record.split())
+    return LoL
+
+def str2numLoL(LoL:list, index:int) -> list:
+    '''Given a list-of-lists and the index of a column in the LoL of stringified numbers as input,
+       return the LoL with that column of values as floats or ints.
+    '''
+    for record in LoL:
+        record[index] = str2num(record[index])
+    return LoL
+
+def getMinMaxLoL(LoL: list, index:int) -> tuple:
+    ''''Given a list-of-lists and the index of a column in the LoL of numbers as input,
+        return a tuple containing the largest and smallest numbers in the column.
+        >>>getMinMaxLoL(LoL, 2) # reformatted_Unigene.fa is the LoL
+        (200, 6616)
+    ''''
+    arry = []
+    for record in LoL:
+        arry.append(record[index])
+    return get_min_max(arry)
