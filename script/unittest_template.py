8f4de1347c250c7a7787c54563d37ee748b8280d
james r. derry <jderry@austin.utexas.edu>
2024-05-27 16:10:04 -0500

    put in dummy strings, meant to be replaced

--- script/unittest_template.py
+++ script/unittest_template.py
@@ -10,23 +10,23 @@ name your actual unit test module "test_<MODULE WE'RE TESTING>.py"
 '''
 
 # 1. get the function to test or import the module to test.
-from myModule import revCompl
+from MODULE import FUNC
 
 # 2. create a class into which we pass the unit tests we want to perform.
-class Test_revCompl(unittest.TestCase):
+class Test_MODULE(unittest.TestCase):
 
    # 3. create a method for each garbage_filter test to perform.    
-   def test_garbage_filter_1(self):
+   def test_GARBAGE_FILTER_1(self):
         ''' assert input must be a string
         '''
         with self.assertRaises(AssertionError):
-            revCompl(45)
+            FUNC(BAD_ARG)
    
    # 4. create a method for each test to perform.
-   def test_getRevCompl(self):
-       result = revCompl("gattaca")
+   def test_FUNC(self):
+       result = FUNC(ARG)
        # here, the test ensures that output of the function is indeed the expected output.
-       self.assertEqual(result, "tgtaatc")
+       self.assertEqual(result, RESULT)
 
 # 5. we run the unit tests defined within the class.
 # we can modify verbosity level (default=1) to get more or less info as tests run. 
